e62e2f7b2abf328834e761e658134b9dcd01dff0
andrew-pineiro <chillsav12345@gmail.com>
2025-03-29 22:59:43 -0400

    improving ls

--- commands.go
+++ commands.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func echo(args []string) {
@@ -20,15 +21,22 @@ func ls() {
 	if len(files) <= 0 {
 		return
 	}
+
 	fmt.Printf("\n  Directory: %s\n\n", dir)
 	fmt.Printf("Mode\t\tModified\t\tSize\tName\n")
 	fmt.Printf("----\t\t--------\t\t----\t----\n")
 	for _, file := range files {
 		file, _ := file.Info()
+		size := strconv.FormatInt(file.Size(), 10)
+		name := file.Name()
+		if file.IsDir() {
+			size = " "
+			name += string(os.PathSeparator)
+		}
 		if file.Name()[0] == '.' {
 			continue
 		}
-		fmt.Printf("%s\t%s\t%d\t%s\n", file.Mode(), file.ModTime().Format("1/2/2006 3:04 PM"), file.Size(), file.Name())
+		fmt.Printf("%s\t%s\t%s\t%s\n", file.Mode(), file.ModTime().Format("1/2/2006 3:04 PM"), size, name)
 	}
 	fmt.Println()
 }
